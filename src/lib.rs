23aab3d2a47b6c5e114b278329b1c8aa8e9666d4
Guillermo Calderon <t-guillermoc@microsoft.com>
2023-06-22 13:33:02 -0700

    Logger changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate log;
 extern crate env_logger;
+use log::{Level, Metadata, Record};
 use cxx::{CxxString};
 
 #[cxx::bridge]
@@ -11,30 +12,51 @@ mod ffi {
     }
 }
 
+// struct NoopLogger
+// {
+//     level: Level,
+// }
+
+// impl log::Log for NoopLogger
+// {
+//     fn enabled(&self, metadata: &Metadata) -> bool
+//     {
+//         true
+//     }
+
+//     fn log(&self, _record: &Record)
+//     {
+//     }
+
+//     fn flush(&self)
+//     {
+//     }
+// }
+
+// impl NoopLogger
+// {
+//     fn init(level: Level)
+//     {
+//         println!(" impl NoopLogger");
+//         log::set_max_level(level.to_level_filter());
+//         let logger = NoopLogger { level };
+//         log::set_boxed_logger(Box::new(logger)).unwrap();
+//     }
+// }
+
 pub fn rust_from_cpp(message: &CxxString) -> () {
     info!("{}", message);
 }
 
 pub fn init_rust_logger() -> ()
 {
-    let logger = Box::new(NoopLogger);
+    // println!("init_rust_logger");
+    // NoopLogger::init(Level::Debug);
+    env_logger::init();
+    // .filter(None, log::LevelFilter::Off)
+    // .init();
+    // let logger = Box::new(NoopLogger { level: Level::Error });
 
-    log::set_boxed_logger(logger).unwrap();
-    log::set_max_level(log::LevelFilter::Trace);
-}
-
-struct NoopLogger;
-
-impl log::Log for NoopLogger {
-    fn enabled(&self, _metadata: &log::Metadata) -> bool {
-        true
-    }
-
-    fn log(&self, _record: &log::Record) {
-        // do nothing
-    }
-
-    fn flush(&self) {
-        // do nothing
-    }
+    // log::set_boxed_logger(logger).unwrap();
+    // log::set_max_level(log::LevelFilter::Info);
 }
